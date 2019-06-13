22fa11e16b1316589bcb0caab205bb718de8cb28
Tripti Naithani <imtriptinaithani@gmail.com>
2019-06-13 09:41:54 +0000

    Upload New File

---
+++ practice/13thJune/largestpalindrome.py
@@ -0,0 +1,8 @@
+def ispalindrome(num):
+    return num == int(str(num)[::-1])
+
+def largest_palindrome():
+    rangee = list(range(999, 99, -1))
+    return max([num1*num2 for num1 in rangee for num2 in rangee if ispalindrome(num1 * num2)])
+
+print(largest_palindrome())
