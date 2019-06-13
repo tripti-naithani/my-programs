a67d3481dd76fb66a2d0c9ba5bff47873c64ab4a
Tripti Naithani <imtriptinaithani@gmail.com>
2019-06-13 06:54:11 +0000

    Update multiples.py

--- practice/13thJune/multiples.py
+++ practice/13thJune/multiples.py
@@ -1,3 +1,4 @@
+#1
 def sum_of_multiples(num1, num2, limit):
     multiples = []
     for i in range(1, limit):
@@ -7,3 +8,6 @@ def sum_of_multiples(num1, num2, limit):
 
 limit = int(input())
 print(sum_of_multiples(3, 5, limit))
+
+#2
+print(sum([i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0]))
